09b85c58d52860546a439e76d93525078e5f8470
Kevin Garner <kevin@kgar.net>
2022-01-17 16:37:15 -0600

    extended gcd

--- general/mathematics.py
+++ general/mathematics.py
@@ -8,3 +8,23 @@ def gcd(a, b):
     return a
 
 print(gcd(66528, 52920))
+
+# Extended GCD
+def ext_gcd(a, b):
+    old_r, r = a, b
+    old_s, s = 1, 0
+    old_t, t = 0, 1
+
+    while r != 0:
+        quot = old_r // r
+        old_r, r = r, old_r - (quot * r)
+        old_s, s = s, old_s - (quot * s)
+        old_t, t = t, old_t - (quot * t)
+
+    return old_s, old_t
+
+
+print("\nExtended GCD:")
+p = 26513
+q = 32321
+print(ext_gcd(p, q))
